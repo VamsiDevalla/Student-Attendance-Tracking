3f7505012b31d9d5bbf26e3f7f643dd29bd08463
VamsiDevalla <S528117@nwmissouri.edu>
2017-09-18 22:01:59 -0500

    initialized web app
    1) organized web app file system according to the type of files.
    2) started developing test application to test cross platform database implimentation

---
+++ WebApp/test.js
@@ -0,0 +1,25 @@
+// var Parse = require('parse');
+// var appId = "4N5weJdvcHqi3tc1o0ciQjs7s0O2ezOkFmXwQi4h";
+// var javaSciptKey = "PBJqn5rA4MbrYR7cFZXeQjqUbFBX7ZYu9zRmgAeW";
+// Parse.initialize(appId,javaSciptKey);
+// Parse.serverURL = 'https://parseapi.back4app.com'
+
+
+// $("button").click(function(){
+// 	var myname=$("input[type='text']").val();
+// 	var mypass= $("input[type='password']").val();
+
+// 	Parse.User.logIn("myname", "mypass", {
+//   success: function(user) {
+//     alert("success");
+//   },
+//   error: function(user, error) {
+//     alert("fail");
+//     console.log(myname);
+//     console.log(mypass);
+//   }
+//   });
+// });
+
+
+
