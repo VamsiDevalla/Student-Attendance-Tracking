e59fc97ccc40679af2d2130677bc301b644b8bb4
VamsiDevalla <S528117@nwmissouri.edu>
2017-09-24 18:05:37 -0500

    Connected to Back4App
    successfully connected student and instructor modules with each other

--- WebApp/test.js
+++ WebApp/test.js
@@ -1,25 +1,31 @@
-// var Parse = require('parse');
-// var appId = "4N5weJdvcHqi3tc1o0ciQjs7s0O2ezOkFmXwQi4h";
-// var javaSciptKey = "PBJqn5rA4MbrYR7cFZXeQjqUbFBX7ZYu9zRmgAeW";
-// Parse.initialize(appId,javaSciptKey);
-// Parse.serverURL = 'https://parseapi.back4app.com'
+var Parse = require('parse/node');
+var express = require("express");
+var app = express(); 
+var http = require('http').Server(app);
+var appId = "HxaUEjDqH5kQDlygLZfTP1meqZ9d9T89kiF9C1wB";
+var javakey = "apfmv9o37gTwYMbYXI5xQMrZGIQjuqagOLsdSzEy";
+Parse.initialize(appId, javakey);
+Parse.serverURL = 'https://parseapi.back4app.com';
+app.get("/",function(req,res){
+    var myname="devalla";
+    var mypass= "devalla";
+    Parse.User.logIn(myname, mypass, {
+      success: function(user) {
+       console.log("success");
+       res.send("Connected to back4app");
+      },
+      error: function(user, error) {
+        console.log("fail");
+         res.send("Failed to connect with back4app");
+      }
+    });
+  
+});
 
+http.listen(8081, function () {
+  console.log('App listening on http://127.0.0.1:8081/');
+});
 
-// $("button").click(function(){
-// 	var myname=$("input[type='text']").val();
-// 	var mypass= $("input[type='password']").val();
-
-// 	Parse.User.logIn("myname", "mypass", {
-//   success: function(user) {
-//     alert("success");
-//   },
-//   error: function(user, error) {
-//     alert("fail");
-//     console.log(myname);
-//     console.log(mypass);
-//   }
-//   });
-// });
 
 
 
