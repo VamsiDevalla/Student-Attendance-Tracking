e2b660ee9de2c7b78b7b064f6b68c79b7f513941
VAMSI DEVALLA <vammichanti40312@gmail.com>
2017-11-15 17:59:12 -0600

    UI changes
    UI changes are mode to provide options for regestration status and to removing students.

--- WebApp/public/JS/controller.js
+++ WebApp/public/JS/controller.js
@@ -1,18 +1,19 @@
 
-       function randomCode(){
-        document.querySelector('#text').value=Math.floor(Math.random()*(999-100+1)+100);
-       }
+function randomCode(){
+    document.querySelector('#text').value=Math.floor(Math.random()*(999-100+1)+100);
+}
         
 
-        function generateBarCode(){
-            var n=document.querySelector('input[type="Number"]').value;
-        for(var i=0;i<n;i++){
-            var nric = document.querySelector('#text').value;
-            var st="";
-            st += nric;
-                var run = $('#ram').val();
-                var url = 'https://api.qrserver.com/v1/create-qr-code/?data=' + st + run + '&amp;size=50x50';
-                $('#barcode').attr('src', url); 
-        }
-        }
+function generateBarCode(){
+  var n=document.querySelector('input[type="Number"]').value;
+  for(var i=0;i<n;i++){
+    var nric = document.querySelector('#text').value;
+    var st="";
+    st += nric;
+    var run = $('#ram').val();
+    var url = 'https://api.qrserver.com/v1/create-qr-code/?data=' + st + run + '&amp;size=50x50';
+    $('#barcode').attr('src', url);
+    $('#uri').val("https://api.qrserver.com/v1/create-qr-code/?data='" + st + run + "'&amp");
+  }
+}
 
